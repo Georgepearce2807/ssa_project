85780e7da81c2a2e14729ef5165d964feaa3bd6e
Georgepearce2807 <georgep2807@gmail.com>
2024-10-31 13:30:47 +1100

    First Lesson Commit

---
+++ chipin/static/chipin/function.js
@@ -0,0 +1,6 @@
+let bed = document.getElementById("bed");
+function shitbed() {
+    let p = document.createElement('p');
+    p.textContent = "you have shit the bed. panic";
+    bed.appendChild(p);
+}
